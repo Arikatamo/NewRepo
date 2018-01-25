bbda675b6ab75f3b897ec9f1e73fc188784c2b34
user <user@A34COMP8>
2018-01-25 20:52:44 +0200

    fff

---
+++ MyEntityFramework/AdminFilter.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyEntityFramework
+{
+    /// <summary>
+    /// Interaction logic for AdminFilter.xaml
+    /// </summary>
+    public partial class AdminFilter : Window
+    {
+        public AdminFilter()
+        {
+            InitializeComponent();
+        }
+    }
+}
